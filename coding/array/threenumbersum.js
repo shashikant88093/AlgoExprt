14f9b0a334769d2d354e2a86d83cecef3b759cf6
shashikant kumar <shashi94goswami@gmail.com>
2024-10-18 15:55:40 +0530

    added with while loop

--- coding/array/threenumbersum.js
+++ coding/array/threenumbersum.js
@@ -1,6 +1,42 @@
 // sample input array = [12,3,1,2,-6,5,-8,6]  targetsum = 0
 // sample output [[-8,2,6],[-8,3,5],[-6,1,5]]
 
+
+// function threeNumberSum(array, targetsum) {
+//     const arr = [];
+//     // Sort the array to use the two-pointer technique
+//     array.sort((a, b) => a - b);
+
+//     for (let i = 0; i < array.length - 2; i++) {
+//         let j = i + 1;
+//         let k = array.length - 1;
+
+//         while (j < k) {
+//             const threesum = array[i] + array[j] + array[k];
+
+//             if (threesum === targetsum) {
+//                 arr.push([array[i], array[j], array[k]]);
+//                 j++;
+//                 k--;
+
+//                 // Avoid duplicates: skip the same values for j and k
+//                 while (j < k && array[j] === array[j - 1]) j++;
+//                 while (j < k && array[k] === array[k + 1]) k--;
+//             } else if (threesum < targetsum) {
+//                 j++; // Increase the sum by moving the left pointer
+//             } else {
+//                 k--; // Decrease the sum by moving the right pointer
+//             }
+//         }
+
+//         // Avoid duplicates for i
+//         while (i < array.length - 2 && array[i] === array[i + 1]) i++;
+//     }
+    
+//     return arr;
+// }
+
+
 function threeNumberSum(array, targetsum) {
   // console.log(array, "array sort")
   let i, j, k, threesum, arr;
@@ -29,3 +65,5 @@ let array = [12, 3, 1, 2, -6, 5, -8, 6];
 
 let targetsum = 0;
 console.log(threeNumberSum(array, targetsum));
+
+
